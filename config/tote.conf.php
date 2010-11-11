a8920196576139c94b1bd300b2a23f08761a8eeb
Chris Han <xiphux@gmail.com>
2010-11-10 23:27:35 -0600

    Initial config and index

---
+++ config/tote.conf.php
@@ -0,0 +1,9 @@
+<?php
+
+$tote_conf['connectionString'] = '';
+
+$tote_conf['database'] = 'tote';
+
+$tote_conf['namespace'] = '';
+
+$tote_conf['smarty'] = 'smarty/';
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('config/tote.conf.php');
+
+$connection = null;
+if (!empty($tote_conf['connectionString']))
+	$connection = new Mongo($tote_conf['connectionString']);
+else
+	$connection = new Mongo();
+$db = $connection->selectDB($tote_conf['database']);
+
+require_once($tote_conf['smarty'] . 'Smarty.class.php');
+$tpl = new Smarty();
+
+switch($_GET['a']) {
+	case 'pool':
+		require_once('include/controller/pool.inc.php');
+		break;
+	case 'bet':
+		require_once('include/controller/bet.inc.php');
+		break;
+	case 'addbet':
+		require_once('include/controller/addbet.inc.php');
+		break;
+	case 'update':
+		require_once('include/controller/update.inc.php');
+		break;
+	default:
+		require_once('include/controller/pool.inc.php');
+		break;
+}
