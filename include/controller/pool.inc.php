df4c542ec5e5f07bce7d0bb073e86d7feadef30a
Chris Han <xiphux@gmail.com>
2010-11-11 22:06:34 -0600

    Initial commit of pool tabulation and display logic

---
+++ include/controller/pool.inc.php
@@ -0,0 +1,180 @@
+<?php
+
+$poolcol = 'pools';
+$usercol = 'users';
+$gamecol = 'games';
+$teamcol = 'teams';
+if (!empty($tote_conf['namespace'])) {
+	$poolcol = $tote_conf['namespace'] . '.' . $poolcol;
+	$usercol = $tote_conf['namespace'] . '.' . $usercol;
+	$gamecol = $tote_conf['namespace'] . '.' . $gamecol;
+	$teamcol = $tote_conf['namespace'] . '.' . $teamcol;
+}
+
+$pools = $db->selectCollection($poolcol);
+$users = $db->selectCollection($usercol);
+$games = $db->selectCollection($gamecol);
+$teams = $db->selectCollection($teamcol);
+
+$teamcache = array();
+
+function get_team($id)
+{
+	global $teamcache, $teams;
+
+	if (empty($teamcache[(string)$id])) {
+		$teamcache[(string)$id] = $teams->findOne(array('_id' => $id));
+	}
+
+	return $teamcache[(string)$id];
+}
+
+function sort_pool($a, $b)
+{
+	// first wins descending
+	if ($a['wins'] != $b['wins'])
+		return ($a['wins'] > $b['wins'] ? -1 : 1);
+
+	// then losses ascending
+	if ($a['losses'] != $b['losses'])
+		return ($a['losses'] > $b['losses'] ? 1 : -1);
+
+	// then spread descending
+	if ($a['spread'] != $b['spread'])
+		return ($a['spread'] > $b['spread'] ? -1 : 1);
+
+	$user1 = $a['user']['username'];
+	if (!empty($a['user']['first_name'])) {
+		$user1 = $a['user']['first_name'];
+		if (!empty($a['user']['last_name']))
+			$user1 .= ' ' . $a['user']['last_name'];
+		$user1 = trim($user1);
+	}
+	$user2 = $b['user']['username'];
+	if (!empty($b['user']['first_name'])) {
+		$user2 = $b['user']['first_name'];
+		if (!empty($b['user']['last_name']))
+			$user2 .= ' ' . $b['user']['last_name'];
+		$user2 = trim($user2);
+	}
+
+	return strcmp($user1, $user2);
+}
+
+$poolobj = null;
+
+if (!empty($_GET['p']))
+	$poolobj = $pools->findOne(array('_id' => $_GET['p']));
+else {
+	// find most recent pool
+	$poolobj = $pools->find()->sort(array('season' => -1))->getNext();
+}
+
+if (!$poolobj) {
+	echo "Pool not found";
+} else {
+	$entries = array();
+
+	$lastgame = $games->find(array('season' => (int)$poolobj['season']))->sort(array('week' => -1))->getNext();
+	$weeks = $lastgame['week'];
+
+	$openweeks = array();
+	$currentdate = new MongoDate(time());
+	for ($i = 1; $i <= $weeks; $i++) {
+		$opengame = $games->findOne(array('season' => $poolobj['season'], 'week' => $i, 'start' => array('$gt' => $currentdate)));
+		$openweeks[$i] = ($opengame ? true : false);
+	}
+
+	$poolrecord = array();
+
+	foreach ($poolobj['entries'] as $entrant) {
+		
+		$record = array();
+		$record['user'] = $users->findOne(array('_id' => $entrant['user']), array('username', 'first_name', 'last_name'));
+		$bets = array();
+		
+		foreach ($entrant['bets'] as $bet) {
+			// remap bets indexed by week
+			$week = $bet['week'];
+			if (!empty($week))
+				$bets[(int)$week] = array('team' => $bet['team']);
+		}
+
+		foreach ($bets as $week => $bet) {
+			// find the result of each bet
+	
+			$js = "function() {
+				return ((this.home_team == '" . $bet['team'] . "') || (this.away_team == '" . $bet['team'] . "'));
+			}";
+
+			$gameobj = $games->findOne(array('season' => $poolobj['season'], 'week' => (int)$week, '$where' => $js));
+
+			if ($gameobj && isset($gameobj['home_score']) && isset($gameobj['away_score'])) {
+				$result = 0;
+				$gamespread = $gameobj['home_score'] - $gameobj['away_score'];
+				if ($gamespread > 0)
+					$result = 1;
+				else if ($gamespread < 0)
+					$result = -1;
+				if ($gameobj['away_team'] == $bet['team'])
+					$result *= -1;
+				$gamespread = abs($gamespread);
+
+				$bets[$week]['result'] = $result;
+				$bets[$week]['spread'] = $gamespread;
+			}
+		
+			// also load team object
+			$bets[$week]['team'] = get_team($bet['team']);
+		}
+
+		$wins = 0;
+		$losses = 0;
+		$pointspread = 0;
+
+		for ($i = 1; $i <= $weeks; ++$i) {
+			// tabulate
+			if (isset($bets[$i])) {
+				// has bet
+				if (isset($bets[$i]['result'])) {
+					if ($bets[$i]['result'] > 0) {
+						$wins++;
+						$pointspread += $bets[$i]['spread'];
+					} else if ($bets[$i]['result'] < 0) {
+						$losses++;
+						$pointspread -= $bets[$i]['spread'];
+					}
+				}
+			} else {
+				// no bet
+				$bets[$i] = array();
+				if (!$openweeks[$i]) {
+					$bets[$i]['nopick'] = true;
+					$bets[$i]['result'] = -1;
+					$losses++;
+
+					if (($weeks - $i) < 4) {
+						// no picks in last 4 weeks is 10 point penalty
+						$bets[$i]['spread'] = 10;
+						$pointspread -= 10;
+					}
+				}
+			}
+		}
+
+		ksort($bets);
+
+		$record['bets'] = $bets;
+		$record['wins'] = $wins;
+		$record['losses'] = $losses;
+		$record['spread'] = $pointspread;
+
+		$poolrecord[] = $record;
+	}
+
+	usort($poolrecord, 'sort_pool');
+
+	$tpl->assign('weeks', $openweeks);
+	$tpl->assign('record', $poolrecord);
+	$tpl->display('pool.tpl');
+}
---
+++ templates/pool.tpl
@@ -0,0 +1,48 @@
+{include file='header.tpl'}
+
+<table class="scoreTable">
+
+<thead>
+<th>Name</th>
+<th title="Wins">W</th>
+<th title="Losses">L</th>
+<th title="Point Spread">PS</th>
+{foreach from=$weeks key=wknum item=open}
+<th title="Week {$wknum}">W{$wknum}</th>
+{/foreach}
+</thead>
+
+<tbody>
+
+{foreach from=$record item=entrant}
+
+<tr class="{cycle values=light,dark}">
+
+<td>{if $entrant.user.first_name}{$entrant.user.first_name} {$entrant.user.last_name}{else}{$entrant.user.username}{/if}</td>
+
+<td>{$entrant.wins}</td>
+<td>{$entrant.losses}</td>
+<td>{$entrant.spread}</td>
+
+{foreach from=$entrant.bets item=bet}
+<td>
+<span {if $bet.result > 0}class="win"{elseif $bet.result < 0}class="loss"{/if}>
+{if $bet.team.abbreviation}
+{$bet.team.abbreviation}
+{elseif $bet.nopick}
+No Pick
+{/if}
+ {if $bet.spread}({$bet.spread}){/if}
+</span>
+</td>
+{/foreach}
+
+</tr>
+
+{/foreach}
+
+</tbody>
+
+</table>
+
+{include file='footer.tpl'}
