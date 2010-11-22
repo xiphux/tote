d815a1c81e37c081ecea5cdf092ae8ac43b6c4ad
Chris Han <xiphux@gmail.com>
2010-11-21 16:42:17 -0800

    Commit production settings

--- config/tote.conf.php
+++ config/tote.conf.php
@@ -1,15 +1,15 @@
 <?php
 
-$tote_conf['connectionString'] = '';
+$tote_conf['connectionString'] = 'mongodb://tote:taxA2t2GeneTrudR@localhost/tote';
 
 $tote_conf['database'] = 'tote';
 
 $tote_conf['namespace'] = '';
 
-$tote_conf['smarty'] = 'smarty/';
+$tote_conf['smarty'] = '/home/xiphux/smarty/';
 
-$tote_conf['sitename'] = 'tote';
+$tote_conf['sitename'] = 'football.xiphux.com';
 
 $tote_conf['fromemail'] = 'xiphux@gmail.com';
 
-$tote_conf['reminders'] = false;
+$tote_conf['reminders'] = true;
