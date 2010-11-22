93a9f09184db1cd3fdfa4283faa1521ea480f8ba
Chris Han <xiphux@gmail.com>
2010-11-21 16:40:52 -0800

    Commit playground settings

--- config/tote.conf.php
+++ config/tote.conf.php
@@ -1,15 +1,15 @@
 <?php
 
-$tote_conf['connectionString'] = '';
+$tote_conf['connectionString'] = 'mongodb://toteplayground:sWU5r9gesweSpeke@localhost/toteplayground';
 
-$tote_conf['database'] = 'tote';
+$tote_conf['database'] = 'toteplayground';
 
 $tote_conf['namespace'] = '';
 
-$tote_conf['smarty'] = 'smarty/';
+$tote_conf['smarty'] = '/home/xiphux/smarty/';
 
-$tote_conf['sitename'] = 'tote';
+$tote_conf['sitename'] = 'Tote playground';
 
 $tote_conf['fromemail'] = 'xiphux@gmail.com';
 
-$tote_conf['reminders'] = false;
+$tote_conf['reminders'] = true;
