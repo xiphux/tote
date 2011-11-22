caec97da25b8a5a3e10ae52df6093c5119c50fdc
Chris Han <xiphux@gmail.com>
2011-11-21 20:35:03 -0600

    Pin schedule toc when scrolling

--- js/fullschedule.js
+++ js/fullschedule.js
@@ -43,6 +43,31 @@ function initScheduleToc() {
 };
 
 function initScheduleTocPin() {
+
+	var toc = $('.scheduleTocContent');
+
+	var tocYLoc = toc.position().top;
+	var tocPosition = toc.css('position');
+	var tocTop = toc.css('top');
+
+	var pinned = false;
+
+	$(window).scroll(function() {
+		var windowYLoc = $(document).scrollTop();
+		if (windowYLoc > tocYLoc) {
+			if (!pinned) {
+				toc.css('position', 'fixed');
+				toc.css('top', '0px');
+				pinned = true;
+			}
+		} else {
+			if (pinned) {
+				toc.css('position', tocPosition);
+				toc.css('top', tocTop);
+				pinned = false;
+			}
+		}
+	});
 };
 
 $(document).ready(function() {
