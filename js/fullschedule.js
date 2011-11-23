f628e5c944a859f370bf03736b3161703967bafc
Chris Han <xiphux@gmail.com>
2011-11-22 18:49:52 -0600

    Fix toc pinning and turn off animations on ie8

--- js/fullschedule.js
+++ js/fullschedule.js
@@ -1,3 +1,19 @@
+function getInternetExplorerVersion() {
+	var rv = -1;
+	if (navigator.appName == 'Microsoft Internet Explorer') {
+		var ua = navigator.userAgent;
+		var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+		if (re.exec(ua) != null)
+			rv = parseFloat(RegExp.$1);
+	}
+	return rv;
+}
+
+function canAnim() {
+	var ver = getInternetExplorerVersion();
+	return (ver === -1) || (ver > 8);
+}
+
 function initSeasonNav() {
 	$('#seasonSelect').change(function() {
 		$(this).parent().submit();
@@ -8,17 +24,22 @@ function initSeasonNav() {
 function initScheduleToc() {
 	var tocContent = $('.scheduleTocContent');
 
+	var anim = canAnim();
+
 	var tocItemClick = function() {
+		if (!mobile) {
+			window.scrollTo(0, 1);
+		}
 		var itemId = $(this).attr('href').substr(1);
 		$('.divScheduleItem').each(function() {
 			if ($(this).attr('id') == itemId) {
-				if (mobile) {
+				if (mobile || !anim) {
 					$(this).show();
 				} else {
 					$(this).slideDown('fast');
 				}
 			} else {
-				if (mobile) {
+				if (mobile || !anim) {
 					$(this).hide();
 				} else {
 					$(this).slideUp('fast');
@@ -31,7 +52,10 @@ function initScheduleToc() {
 	};
 
 	var tocShowAllClick = function() {
-		if (mobile) {
+		if (!mobile) {
+			window.scrollTo(0, 1);
+		}
+		if (mobile || !anim) {
 			$('.divScheduleItem').show();
 		} else {
 			$('.divScheduleItem').slideDown('fast');
