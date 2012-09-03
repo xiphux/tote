1ac1e44d20d4655e9030084f6d817ccba350d5e7
Chris Han <xiphux@gmail.com>
2012-09-02 21:26:22 -0500

    Build gzip versions when minifying for serving gzipped static content

--- util/minify.sh
+++ util/minify.sh
@@ -16,31 +16,69 @@ COMPRESSORJAR="yuicompressor-2.4.2.jar"
 
 JSEXT=".js"
 MINEXT=".min.js"
+GZEXT=".gz"
 
 CSSDIR="css"
 SKINDIR="css/skin"
 CSSEXT=".css"
 MINCSSEXT=".min.css"
 
-rm -f ${JSDIR}/*${MINEXT}
+rm -fv ${JSDIR}/*${MINEXT}
+rm -fv ${JSDIR}/*${GZEXT}
+rm -fv ${JSDIR}/ext/*${GZEXT}
+rm -fv ${CSSDIR}/*${MINCSSEXT}
+rm -fv ${CSSDIR}/*${GZEXT}
+rm -fv ${CSSDIR}/ext/*${GZEXT}
+rm -fv ${SKINDIR}/*/*${MINCSSEXT}
+rm -fv ${SKINDIR}/*/*${GZEXT}
+
+if [ "$1" == "clean" ]; then
+	exit;
+fi
 
 for i in ${JSDIR}/*${JSEXT}; do
 	echo "Minifying ${i}..."
-	java -jar "${COMPRESSORDIR}/${COMPRESSORJAR}" --charset utf-8 -o "${i%$JSEXT}${MINEXT}" "${i}"
+	JSMODULE=${i%$JSEXT}
+	java -jar "${COMPRESSORDIR}/${COMPRESSORJAR}" --charset utf-8 -o "${JSMODULE}${MINEXT}" "${i}"
 done
-
-rm -f ${CSSDIR}/*${MINCSSEXT}
 
 for i in ${CSSDIR}/*${CSSEXT}; do
 	echo "Minifying ${i}..."
-	java -jar "${COMPRESSORDIR}/${COMPRESSORJAR}" --charset utf-8 -o "${i%$CSSEXT}${MINCSSEXT}" "${i}"
+	CSSFILE=${i%$CSSEXT}
+	java -jar "${COMPRESSORDIR}/${COMPRESSORJAR}" --charset utf-8 -o "${CSSFILE}${MINCSSEXT}" "${i}"
 done
-
-rm -f ${SKINDIR}/*/*${MINCSSEXT}
 
 for i in ${SKINDIR}/*; do
 	for j in ${i}/*${CSSEXT}; do
 		echo "Minifying ${j}..."
-		java -jar "${COMPRESSORDIR}/${COMPRESSORJAR}" --charset utf-8 -o "${j%$CSSEXT}${MINCSSEXT}" "${j}"
+		SKINCSSFILE=${j%$CSSEXT}
+		java -jar "${COMPRESSORDIR}/${COMPRESSORJAR}" --charset utf-8 -o "${SKINCSSFILE}${MINCSSEXT}" "${j}"
+	done
+done
+
+for i in ${JSDIR}/*${MINEXT}; do
+	gzip -v -c ${i} > ${i}${GZEXT}
+	touch ${i} ${i}${GZEXT}
+done
+
+for i in ${JSDIR}/ext/*${MINEXT}; do
+	gzip -v -c ${i} > ${i}${GZEXT}
+	touch ${i} ${i}${GZEXT}
+done
+
+for i in ${CSSDIR}/*${MINCSSEXT}; do
+	gzip -v -c ${i} > ${i}${GZEXT}
+	touch ${i} ${i}${GZEXT}
+done
+
+for i in ${CSSDIR}/ext/*${MINCSSEXT}; do
+	gzip -v -c ${i} > ${i}${GZEXT}
+	touch ${i} ${i}${GZEXT}
+done
+
+for i in ${SKINDIR}/*; do
+	for j in ${i}/*${MINCSSEXT}; do
+		gzip -v -c ${j} > ${j}${GZEXT}
+		touch ${j} ${j}${GZEXT}
 	done
 done
