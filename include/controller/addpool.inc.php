17997aa9cec38efb8f3bc26fde84723471fc6f54
Chris Han <xiphux@gmail.com>
2013-09-11 22:10:20 -0500

    Use sql for pool addition/deletion

--- include/controller/addpool.inc.php
+++ include/controller/addpool.inc.php
@@ -2,14 +2,11 @@
 
 require_once(TOTE_INCLUDEDIR . 'validate_csrftoken.inc.php');
 require_once(TOTE_INCLUDEDIR . 'redirect.inc.php');
-require_once(TOTE_INCLUDEDIR . 'get_collection.inc.php');
 require_once(TOTE_INCLUDEDIR . 'user_logged_in.inc.php');
 require_once(TOTE_INCLUDEDIR . 'user_is_admin.inc.php');
 require_once(TOTE_INCLUDEDIR . 'http_headers.inc.php');
 require_once(TOTE_INCLUDEDIR . 'get_seasons.inc.php');
 require_once(TOTE_CONTROLLERDIR . 'message.inc.php');
-
-require_once(TOTE_INCLUDEDIR . 'user_readable_name.inc.php');  // TODO remove this
 
 define('ADDPOOL_HEADER', 'Add A New Pool');
 
@@ -25,7 +22,7 @@ define('ADDPOOL_HEADER', 'Add A New Pool');
  */
 function display_addpool($name, $season, $fee, $csrftoken)
 {
-	global $tpl;
+	global $tpl, $mysqldb;
 
 	$user = user_logged_in();
 	if (!$user) {
@@ -43,8 +40,6 @@ function display_addpool($name, $season, $fee, $csrftoken)
 		return;
 	}
 
-	$pools = get_collection(TOTE_COLLECTION_POOLS);
-
 	$errors = array();
 	if (empty($name)) {
 		// need a pool name
@@ -57,10 +52,14 @@ function display_addpool($name, $season, $fee, $csrftoken)
 		if (!is_numeric($season)) {
 			$errors[] = "Season must be a year";
 		} else {
-			$existingpool = $pools->findOne(
-				array('name' => $name, 'season' => (int)$season),
-				array('name', 'season')
-			);
+			$existingstmt = $mysqldb->prepare('SELECT pools.id FROM ' . TOTE_TABLE_POOLS . ' AS pools LEFT JOIN ' . TOTE_TABLE_SEASONS . ' AS seasons ON seasons.id=pools.season_id WHERE pools.name=? AND seasons.year=?');
+			$intseason = (int)$season;
+			$existingstmt->bind_param('si', $name, $intseason);
+			$existingid = null;
+			$existingstmt->bind_result($existingid);
+			$existingstmt->execute();
+			$existingpool = $existingstmt->fetch();
+			$existingstmt->close();
 			if ($existingpool) {
 				// don't allow duplicate pool names - not for technical reasons,
 				// just because it makes no sense since you can't differentiate
@@ -91,53 +90,92 @@ function display_addpool($name, $season, $fee, $csrftoken)
 		$tpl->assign('csrftoken', $_SESSION['csrftoken']);
 		$tpl->display('newpool.tpl');
 	} else {
-		$data = array(
-			'name' => $name,
-			'season' => (int)$season
-		);
 		if (!empty($fee)) {
 			$fee = round((float)$fee, 2);
-			if ($fee > 0) {
-				$data['fee'] = $fee;
+			if ($fee < 0) {
+				$fee = null;
 			}
-		}
+		} else
+			$fee = null;
+
+		$season = (int)$season;
+
+		$newpoolstmt = $mysqldb->prepare('INSERT INTO ' . TOTE_TABLE_POOLS . ' (season_id, fee, name) VALUES ((SELECT id FROM seasons WHERE year=?),?,?)');
+		$newpoolstmt->bind_param('ids', $season, $fee, $name);
+		$newpoolstmt->execute();
+
+		$poolid = $mysqldb->insert_id;
+
+		$newpoolstmt->close();
 
 		// TODO: make payouts user entered rather than hardcoding
-		$data['payout'] = array(
-			array(
-				'max' => 29,
-				'percents' => array(
-					0.75,
-					0.15,
-					0.10
-				)
-			),
-			array(
-				'min' => 30,
-				'max' => 39,
-				'percents' => array(
-					0.75,
-					0.15,
-					0.10,
-					0
-				)
-			),
-			array(
-				'min' => 40,
-				'percents' => array(
-					0.73,
-					0.13,
-					0.08,
-					0.06,
-					0
-				)
-			)
-		);
+		$newpayoutstmt = $mysqldb->prepare('INSERT INTO ' . TOTE_TABLE_POOL_PAYOUTS . ' (pool_id, minimum, maximum) VALUES (?, ?, ?)');
+
+		// 0-29: 75%, 15%, 10%
+		$min = null;
+		$max = 29;
+		$newpayoutstmt->bind_param('iii', $poolid, $min, $max);
+		$newpayoutstmt->execute();
+		$payoutid = $mysqldb->insert_id;
+
+		$newpercentstmt = $mysqldb->prepare('INSERT INTO ' . TOTE_TABLE_POOL_PAYOUT_PERCENTS . ' (payout_id, place, percent) VALUES (?, ?, ?), (?, ?, ?), (?, ?, ?)');
+		$place1 = 1;
+		$percent1 = 0.75;
+		$place2 = 2;
+		$percent2 = 0.15;
+		$place3 = 3;
+		$percent3 = 0.10;
+		$newpercentstmt->bind_param('iidiidiid', $payoutid, $place1, $percent1, $payoutid, $place2, $percent2, $payoutid, $place3, $percent3);
+		$newpercentstmt->execute();
+		$newpercentstmt->close();
+
+		// 30-39: 75%, 15%, 10%, entry fee
+		$min = 30;
+		$max = 39;
+		$newpayoutstmt->bind_param('iii', $poolid, $min, $max);
+		$newpayoutstmt->execute();
+		$payoutid = $mysqldb->insert_id;
+
+		$newpercentstmt = $mysqldb->prepare('INSERT INTO ' . TOTE_TABLE_POOL_PAYOUT_PERCENTS . ' (payout_id, place, percent) VALUES (?, ?, ?), (?, ?, ?), (?, ?, ?), (?, ?, ?)');
+		$place1 = 1;
+		$percent1 = 0.75;
+		$place2 = 2;
+		$percent2 = 0.15;
+		$place3 = 3;
+		$percent3 = 0.10;
+		$place4 = 4;
+		$percent4 = 0;
+		$newpercentstmt->bind_param('iidiidiidiid', $payoutid, $place1, $percent1, $payoutid, $place2, $percent2, $payoutid, $place3, $percent3, $payoutid, $place4, $percent4);
+		$newpercentstmt->execute();
+		$newpercentstmt->close();
+
+		// 40+: 73%, 13%, 8%, 6%, entry fee
+		$min = 40;
+		$max = null;
+		$newpayoutstmt->bind_param('iii', $poolid, $min, $max);
+		$newpayoutstmt->execute();
+		$payoutid = $mysqldb->insert_id;
+
+		$newpercentstmt = $mysqldb->prepare('INSERT INTO ' . TOTE_TABLE_POOL_PAYOUT_PERCENTS . ' (payout_id, place, percent) VALUES (?, ?, ?), (?, ?, ?), (?, ?, ?), (?, ?, ?), (?, ?, ?)');
+		$place1 = 1;
+		$percent1 = 0.73;
+		$place2 = 2;
+		$percent2 = 0.13;
+		$place3 = 3;
+		$percent3 = 0.08;
+		$place4 = 4;
+		$percent4 = 0.06;
+		$place5 = 5;
+		$percent5 = 0;
+		$newpercentstmt->bind_param('iidiidiidiidiid', $payoutid, $place1, $percent1, $payoutid, $place2, $percent2, $payoutid, $place3, $percent3, $payoutid, $place4, $percent4, $payoutid, $place5, $percent5);
+		$newpercentstmt->execute();
+		$newpercentstmt->close();
+
+		$newpayoutstmt->close();
+
 		// end TODO
 
-		$pools->insert($data);
-
 		// go to the new pool
-		redirect(array('p' => (string)$data['_id']));
+		redirect(array('p' => $poolid));
 	}
 }
--- include/controller/deletepool.inc.php
+++ include/controller/deletepool.inc.php
@@ -2,7 +2,6 @@
 
 require_once(TOTE_INCLUDEDIR . 'validate_csrftoken.inc.php');
 require_once(TOTE_INCLUDEDIR . 'redirect.inc.php');
-require_once(TOTE_INCLUDEDIR . 'get_collection.inc.php');
 require_once(TOTE_INCLUDEDIR . 'user_logged_in.inc.php');
 require_once(TOTE_INCLUDEDIR . 'user_is_admin.inc.php');
 require_once(TOTE_INCLUDEDIR . 'clear_cache.inc.php');
@@ -20,7 +19,7 @@ define('DELETEPOOL_HEADER', 'Manage Your Pool');
  */
 function display_deletepool($poolid, $csrftoken)
 {
-	global $tpl;
+	global $tpl, $mysqldb;
 
 	$user = user_logged_in();
 	if (!$user) {
@@ -44,14 +43,41 @@ function display_deletepool($poolid, $csrftoken)
 		return;
 	}
 
-	$pools = get_collection(TOTE_COLLECTION_POOLS);
+	$removeactionstmt = $mysqldb->prepare('DELETE FROM ' . TOTE_TABLE_POOL_ACTIONS . ' WHERE pool_id=?');
+	$removeactionstmt->bind_param('i', $poolid);
+	$removeactionstmt->execute();
+	$removeactionstmt->close();
 
-	// remove pool from system
-	$pools->remove(
-		array(
-			'_id' => new MongoId($poolid)
-		)
-	);
+	$removeadminstmt = $mysqldb->prepare('DELETE FROM ' . TOTE_TABLE_POOL_ADMINISTRATORS . ' WHERE pool_id=?');
+	$removeadminstmt->bind_param('i', $poolid);
+	$removeadminstmt->execute();
+	$removeadminstmt->close();
+
+	$removepickstmt = $mysqldb->prepare('DELETE pool_entry_picks FROM ' . TOTE_TABLE_POOL_ENTRY_PICKS . ' AS pool_entry_picks LEFT JOIN ' . TOTE_TABLE_POOL_ENTRIES . ' AS pool_entries ON pool_entry_picks.pool_entry_id=pool_entries.id WHERE pool_entries.pool_id=?');
+	$removepickstmt->bind_param('i', $poolid);
+	$removepickstmt->execute();
+	$removepickstmt->close();
+
+	$removeentrystmt = $mysqldb->prepare('DELETE FROM ' . TOTE_TABLE_POOL_ENTRIES . ' WHERE pool_id=?');
+	$removeentrystmt->bind_param('i', $poolid);
+	$removeentrystmt->execute();
+	$removeentrystmt->close();
+
+	$removepercentstmt = $mysqldb->prepare('DELETE pool_payout_percents FROM ' . TOTE_TABLE_POOL_PAYOUT_PERCENTS . ' AS pool_payout_percents LEFT JOIN ' . TOTE_TABLE_POOL_PAYOUTS . ' AS pool_payouts ON pool_payout_percents.payout_id=pool_payouts.id WHERE pool_payouts.pool_id=?');
+	$removepercentstmt->bind_param('i', $poolid);
+	$removepercentstmt->execute();
+	$removepercentstmt->close();
+
+	$removepayoutstmt = $mysqldb->prepare('DELETE FROM ' . TOTE_TABLE_POOL_PAYOUTS . ' WHERE pool_id=?');
+	$removepayoutstmt->bind_param('i', $poolid);
+	$removepayoutstmt->execute();
+	$removepayoutstmt->close();
+
+	$removepoolstmt = $mysqldb->prepare('DELETE FROM ' . TOTE_TABLE_POOLS . ' WHERE id=?');
+	$removepoolstmt->bind_param('i', $poolid);
+	$removepoolstmt->execute();
+	$removepoolstmt->close();
+
 	clear_cache('pool|' . $poolid);
 
 	redirect();
