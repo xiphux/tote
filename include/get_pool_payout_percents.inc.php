bcc7b70f9edc1be225fbcef6c689bff5d50a43fa
Chris Han <xiphux@gmail.com>
2013-09-15 16:47:33 -0500

    Reduce payout percent calculation to a single sql query

--- include/get_pool_payout_percents.inc.php
+++ include/get_pool_payout_percents.inc.php
@@ -15,18 +15,10 @@ function get_pool_payout_percents($poolid)
 	if (empty($poolid))
 		return null;
 
-	$entrantcount = 0;
-	$entrantcountstmt = $mysqldb->prepare('SELECT COUNT(id) FROM ' . TOTE_TABLE_POOL_ENTRIES . ' WHERE pool_id=?');
-	$entrantcountstmt->bind_param('i', $poolid);
-	$entrantcountstmt->bind_result($entrantcount);
-	$entrantcountstmt->execute();
-	$entrantcountstmt->fetch();
-	$entrantcountstmt->close();
-
 	$percents = array();
 
-	$percentstmt = $mysqldb->prepare('SELECT place, percent FROM ' . TOTE_TABLE_POOL_PAYOUT_PERCENTS . ' WHERE payout_id=(SELECT id FROM ' . TOTE_TABLE_POOL_PAYOUTS . ' WHERE pool_id=? AND ((minimum IS NULL) OR (minimum<=?)) AND ((maximum IS NULL) OR (maximum>=?)))');
-	$percentstmt->bind_param('iii', $poolid, $entrantcount, $entrantcount);
+	$percentstmt = $mysqldb->prepare('SELECT place, percent FROM ' . TOTE_TABLE_POOL_PAYOUT_PERCENTS . ' WHERE payout_id=(SELECT id FROM ' . TOTE_TABLE_POOL_PAYOUTS . ' WHERE pool_id=? AND (SELECT COUNT(id) FROM ' . TOTE_TABLE_POOL_ENTRIES . ' WHERE pool_id=?) BETWEEN COALESCE(minimum,0) AND COALESCE(maximum,65535))');
+	$percentstmt->bind_param('ii', $poolid, $poolid);
 	$percentstmt->execute();
 	$percentresult = $percentstmt->get_result();
 
