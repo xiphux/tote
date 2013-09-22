32d9be08b010c238747adfdd429f49328a816917
Chris Han <xiphux@gmail.com>
2013-09-21 23:10:28 -0500

    Use PDO for pool administrators

--- include/get_pool_administrators.inc.php
+++ include/get_pool_administrators.inc.php
@@ -10,19 +10,18 @@
  */
 function get_pool_administrators($poolid)
 {
-	global $mysqldb;
+	global $db;
 
 	if (empty($poolid))
 		return null;
 
-	$adminstmt = $mysqldb->prepare("SELECT pool_administrators.user_id, pool_administrators.name, pool_administrators.admin_type, (CASE WHEN (users.first_name IS NOT NULL AND users.last_name IS NOT NULL) THEN CONCAT(CONCAT(users.first_name,' '),users.last_name) WHEN users.first_name IS NOT NULL THEN users.first_name ELSE users.username END) AS display_name FROM " . TOTE_TABLE_POOL_ADMINISTRATORS . " AS pool_administrators LEFT JOIN " . TOTE_TABLE_USERS . " AS users ON pool_administrators.user_id=users.id WHERE pool_administrators.pool_id=?");
-	$adminstmt->bind_param('i', $poolid);
+	$adminstmt = $db->prepare("SELECT pool_administrators.user_id, pool_administrators.name, pool_administrators.admin_type, (CASE WHEN (users.first_name IS NOT NULL AND users.last_name IS NOT NULL) THEN CONCAT(CONCAT(users.first_name,' '),users.last_name) WHEN users.first_name IS NOT NULL THEN users.first_name ELSE users.username END) AS display_name FROM " . TOTE_TABLE_POOL_ADMINISTRATORS . " AS pool_administrators LEFT JOIN " . TOTE_TABLE_USERS . " AS users ON pool_administrators.user_id=users.id WHERE pool_administrators.pool_id=:pool_id");
+	$adminstmt->bindParam(':pool_id', $poolid, PDO::PARAM_INT);
 	$adminstmt->execute();
-	$adminresult = $adminstmt->get_result();
 
 	$admins = array();
 
-	while ($admin = $adminresult->fetch_assoc()) {
+	while ($admin = $adminstmt->fetch(PDO::FETCH_ASSOC)) {
 		if ($admin['admin_type'] == 2) {
 			$admins['secondary'][] = $admin;
 		} else if ($admin['admin_type'] == 1) {
@@ -30,8 +29,7 @@ function get_pool_administrators($poolid)
 		}
 	}
 
-	$adminresult->close();
-	$adminstmt->close();
+	$adminstmt = null;
 
 	if (count($admins) > 0)
 		return $admins;
