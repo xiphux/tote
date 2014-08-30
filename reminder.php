51dd2519f7c6fdd25805723687333eb38f123fa8
Chris Han <xiphux@gmail.com>
2014-08-30 00:39:45 -0500

    Update reminder script to use shared db init code

--- reminder.php
+++ reminder.php
@@ -24,27 +24,10 @@ if (!empty($tote_conf['reminders']) && ($tote_conf['reminders'] == true)) {
 
 	// create Smarty
 	require_once($tote_conf['smarty'] . 'Smarty.class.php');
+	$tpl = new Smarty();
+	$tpl->plugins_dir[] = TOTE_INCLUDEDIR . 'smartyplugins';
 
-	// create MySQL connection
-	$db = new PDO(sprintf('mysql:host=%s;dbname=%s', $tote_conf['hostname'], $tote_conf['sql_database']), $tote_conf['username'], $tote_conf['password']);
-
-	// define MySQL tables
-	define('TOTE_TABLE_CONFERENCES', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'conferences');
-	define('TOTE_TABLE_DIVISIONS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'divisions');
-	define('TOTE_TABLE_TEAMS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'teams');
-	define('TOTE_TABLE_SEASONS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'seasons');
-	define('TOTE_TABLE_GAMES', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'games');
-	define('TOTE_TABLE_USERS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'users');
-	define('TOTE_TABLE_POOLS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pools');
-	define('TOTE_TABLE_POOL_ENTRIES', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pool_entries');
-	define('TOTE_TABLE_POOL_ENTRY_PICKS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pool_entry_picks');
-	define('TOTE_TABLE_POOL_ACTIONS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pool_actions');
-	define('TOTE_TABLE_POOL_PAYOUTS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pool_payouts');
-	define('TOTE_TABLE_POOL_PAYOUT_PERCENTS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pool_payout_percents');
-	define('TOTE_TABLE_POOL_ADMINISTRATORS', (!empty($tote_conf['prefix']) ? $tote_conf['prefix'] : '') . 'pool_administrators');
-
-
-	$tpl = new Smarty;
+	require_once(TOTE_INCLUDEDIR . 'db.inc.php');
 
 	send_reminders();
 
