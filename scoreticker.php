3404d3588863dd7f7f7efc94cef34330ff730bc5
Chris Han <christopher.f.han@gmail.com>
2019-03-03 12:52:55 -0600

    Remove scoreticker caching option

--- scoreticker.php
+++ scoreticker.php
@@ -25,26 +25,10 @@ $mtime = curl_getinfo($ch, CURLINFO_FILETIME);
 
 $raw = null;
 
-if ($mtime > 0 && isset($tote_conf['cache']) && ($tote_conf['cache'] == true)) {
-	if (is_readable(TOTE_CACHEDIR . 'scoreticker.dat')) {
-		$cached = file_get_contents(TOTE_CACHEDIR . 'scoreticker.dat');
-		if ($cached) {
-			$cachedtime = strtok($cached, "\n");
-			if ((int)$cachedtime >= $mtime) {
-				$raw = substr($cached, strlen($cachedtime) + 1);
-			}
-		}
-	}
-}
 if (empty($raw)) {
 	curl_setopt($ch, CURLOPT_HTTPGET, 1);
 
 	$raw = curl_exec($ch);
-
-	if (!empty($raw) && isset($tote_conf['cache']) && ($tote_conf['cache'] == true)) {
-		$data = $mtime . "\n" . $raw;
-		file_put_contents(TOTE_CACHEDIR . 'scoreticker.dat', $data, LOCK_EX);
-	}
 }
 
 curl_close($ch);
