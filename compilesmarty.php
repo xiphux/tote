019e5bd55d6e946ac4b828bdb8ea6d3503829797
Chris Han <christopher.f.han@gmail.com>
2019-03-03 12:31:31 -0600

    Create script to precompile smarty templates

---
+++ compilesmarty.php
@@ -0,0 +1,21 @@
+<?php
+
+// define include directories
+define('TOTE_BASEDIR', dirname(__FILE__) . '/');
+define('TOTE_CONFIGDIR', TOTE_BASEDIR . 'config/');
+define('TOTE_INCLUDEDIR', TOTE_BASEDIR . 'include/');
+define('TOTE_CONTROLLERDIR', TOTE_INCLUDEDIR . 'controller/');
+
+require_once(TOTE_CONFIGDIR . 'tote.conf.php');
+
+// create Smarty
+require_once($tote_conf['smarty'] . 'Smarty.class.php');
+$tpl = new Smarty();
+$tpl->plugins_dir[] = TOTE_INCLUDEDIR . 'smartyplugins';
+
+date_default_timezone_set('UTC');
+
+foreach (glob('templates/*.tpl') as $template) {
+    $templatefile = basename($template);
+    $data = $tpl->fetch($templatefile);
+}
