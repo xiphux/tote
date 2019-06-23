0765350c32522d35f5ab498e7656ef1958288b40
Chris Han <christopher.f.han@gmail.com>
2019-06-23 11:16:13 -0500

    Remove unused include

--- include/controller/update.inc.php
+++ include/controller/update.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(TOTE_INCLUDEDIR . 'update_games.inc.php');
-require_once(TOTE_INCLUDEDIR . 'import_point_spreads.inc.php');
+//require_once(TOTE_INCLUDEDIR . 'import_point_spreads.inc.php');
 require_once(TOTE_INCLUDEDIR . 'http_headers.inc.php');
 
 http_headers();
