e68da250758416f18d94b582b7d4aaf290417e95
Chris Han <christopher.f.han@gmail.com>
2019-06-23 11:16:39 -0500

    Use standard file streams for retrieving urls

--- include/load_page.inc.php
+++ include/load_page.inc.php
@@ -8,15 +8,6 @@
  */
 function load_page($url)
 {
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	ob_start();
-	curl_exec($ch);
-	curl_close($ch);
-	$raw = ob_get_contents();
-	ob_end_clean();
-
-	return $raw;
+	return file_get_contents($url);
 }
 
